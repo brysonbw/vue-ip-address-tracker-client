e9b6da007277901cfa47cb6c3d0b8e39c34bb331
brysonbw <brysonward77@gmail.com>
2024-08-12 15:25:53 -0400

    clean up on app created method/checks

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,45 @@
 <script lang="ts" setup>
   import { useAppStore } from './stores/app'
   import { useToastStore } from './stores/toast'
+
+  // Stores
   const appStore = useAppStore()
   const toastStore = useToastStore()
 
-  // On init ask user for permission to know location
-  async function requestLocation () {
+  /**
+   * On App init (before component mounts)
+   * @usage Init API calls, listeners, checks, ect...
+   */
+  async function onCreated () {
+    await detectWebGL().then(async isWebGLEnabled => {
+      console.log('is WebGL enabled: ', isWebGLEnabled)
+      if (isWebGLEnabled) {
+        appStore.webglEnabled = true
+        await requestGeolocation()
+      } else {
+        const message = 'Your browser or device may not support WebGL.'
+        toastStore.initToast({ message, type: 'error', duration: 9000 })
+        throw new Error(message)
+      }
+    }).catch(error => {
+      console.error(error)
+    })
+  }
+
+  /**
+   * Detect if WebGL enabled for browser
+   */
+  async function detectWebGL () {
+    const canvas = document.createElement('canvas')
+    const gl =
+      canvas.getContext('webgl') || canvas.getContext('experimental-webgl')
+    return gl instanceof WebGLRenderingContext
+  }
+
+  /**
+   * Request user Geolocation
+   */
+  async function requestGeolocation () {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(success, error)
     } else {
@@ -31,5 +65,5 @@
     toastStore.initToast({ message: 'Geolocation access denied. See your browser settings to allow access.', type: 'info', duration: 9000 })
   }
 
-  requestLocation()
+  onCreated()
 </script>
